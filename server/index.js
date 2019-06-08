055b1943917c815e021ff07240e35af07350d18e
Geecek <gicalamaciej@gmail.com>
2019-06-08 16:56:57 +0200

    init server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const port = new SerialPort('COM8', { baudRate: 38400 })
+
+const express = require('express')
+const app = express()
+
+const server = app.listen(3000, () => {
+    console.log('Server running on port 3000')
+})
+
+const io = require('socket.io')(server)
+
+const parser = new Readline()
+port.pipe(parser)
+
+parser.on('data', line => console.log(line))
+
